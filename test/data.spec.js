50ea97fe361ed48fb4395be2462ae24088cb939b
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-07-30 11:40:48 -0500

    add changes in readme.md

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,16 @@
 // importamos la función `example`
 import cargarJSON from "../src/helpers/cargarjsonone.js";
 
+
 describe('example', () => {
 
     it('debería ser una función', () => {
-        expect(typeof cargarJSON).toBe('function');
+        expect(typeof cargarJSON.encode).toBe('function');
     });
 
-    describe('example', () => {
-        // escribe aquí tu test
-    });
+    // describe('example', () => {
+    //     it('debería ', () => {
+    //         expect(typeof cargarJSON.encode).toBe('function');
+    //     });
+    // });
 });
